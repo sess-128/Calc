24ac6c442aa88f9791476113758cc8e57e4a11a6
andrew <rrtyuisess@gmail.com>
2023-07-01 01:27:53 +0300

    First try calc

--- src/Main.java
+++ src/Main.java
@@ -6,67 +6,118 @@ public class Main {
     public static void main(String[] args) throws Exception{
 
         // Ввод данных
-
+        Calc calculator = new Calc();
         Scanner userInput = new Scanner(System.in);
         String userInputString = userInput.nextLine();
         String[] collected = userInputString.split(" ");
+        calculator.analyzer(collected);
 
-        // Проверка инпута на длину
 
-        if (collected.length != 3) throw new Exception("Строка должна содержать" +
-                " два операнда и арифметичский знак") {
-            };
-        // Массивы чисел для проверки
-        String[] arabicNumbers = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-        String[] romanNumbers = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 //
-        // Проверяем полученные данные на вхождение в массивы
-        String searchedNumber1 = collected[0];
-        String searchedNumber2 = collected[2];
-//        // Арабские
-        boolean checkedArabic1 = Arrays.asList(arabicNumbers).contains(searchedNumber1);
-        boolean checkedArabic2 = Arrays.asList(arabicNumbers).contains(searchedNumber2);
-        boolean goodA = checkedArabic2 && checkedArabic1;
-
-        // Римские
-        boolean checkedRoman1 = Arrays.asList(romanNumbers).contains(searchedNumber1);
-        boolean checkedRoman2 = Arrays.asList(romanNumbers).contains(searchedNumber2);
-        boolean goodR = checkedRoman1 && checkedRoman2;
-
-        // Проверка на ввод чисел из одной системы счисления
-        if (goodA || goodR) {
-            System.out.println("Все гуд");
-        } else {
-            throw new Exception("Числа должны быть разных систем счисления");
-        }
-
-        String operand = collected[1];
-        if (goodA){
-            int first = Integer.parseInt(collected[0]);
-            int second = Integer.parseInt(collected[2]);
-            if ((first < 1 || first >10) || (second<1 || second >10)){
-                throw new Exception("Out of bounds");
-            } else {
-                Calc arabic = new Calc();
-                int result = arabic.calculateA(first, second, operand);
-                System.out.println(result);
-            }
-        } else if (goodR) {
-            Calc roman = new Calc();
-            int first = roman.romanArabic(collected[0]);
-            int second = roman.romanArabic(collected[2]);
-            if ((first < 1 || first >10) || (second<1 || second >10)){
-                throw new Exception("Out of bounds");
-            } else {
-            int toRoman = roman.calculateR(first,second,operand);
-            String result = roman.arabicRoman(toRoman);
-            System.out.println(result);
-            }
-// hello git again
-        }
+//        // Проверка инпута на длину
+//
+//        if (collected.length != 3) throw new Exception("Строка должна содержать" +
+//                " два операнда и арифметичский знак") {
+//            };
+//        // Массивы чисел для проверки
+//        String[] arabicNumbers = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+//        String[] romanNumbers = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+////
+//        // Проверяем полученные данные на вхождение в массивы
+//        String searchedNumber1 = collected[0];
+//        String searchedNumber2 = collected[2];
+////        // Арабские
+//        boolean checkedArabic1 = Arrays.asList(arabicNumbers).contains(searchedNumber1);
+//        boolean checkedArabic2 = Arrays.asList(arabicNumbers).contains(searchedNumber2);
+//        boolean goodA = checkedArabic2 && checkedArabic1;
+//
+//        // Римские
+//        boolean checkedRoman1 = Arrays.asList(romanNumbers).contains(searchedNumber1);
+//        boolean checkedRoman2 = Arrays.asList(romanNumbers).contains(searchedNumber2);
+//        boolean goodR = checkedRoman1 && checkedRoman2;
+//
+//        // Проверка на ввод чисел из одной системы счисления
+//        if (goodA || goodR) {
+//            System.out.println("Все гуд");
+//        } else {
+//            throw new Exception("Числа должны быть разных систем счисления");
+//        }
+//
+//        String operand = collected[1];
+//        if (goodA){
+//            int first = Integer.parseInt(collected[0]);
+//            int second = Integer.parseInt(collected[2]);
+//            if ((first < 1 || first >10) || (second<1 || second >10)){
+//                throw new Exception("Out of bounds");
+//            } else {
+//                Calc arabic = new Calc();
+//                int result = arabic.calculateA(first, second, operand);
+//                System.out.println(result);
+//            }
+//        } else if (goodR) {
+//            Calc roman = new Calc();
+//            int first = roman.romanArabic(collected[0]);
+//            int second = roman.romanArabic(collected[2]);
+//            if ((first < 1 || first >10) || (second<1 || second >10)){
+//                throw new Exception("Out of bounds");
+//            } else {
+//            int toRoman = roman.calculateR(first,second,operand);
+//            String result = roman.arabicRoman(toRoman);
+//            System.out.println(result);
+//            }
+//// hello git again
+//        }
     }
 }
 class Calc {
+    String[] arabicNumbers = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    String[] romanNumbers = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    void analyzer(String[] userInput) throws Exception {
+//        if (userInput.length != 3) throw new Exception("Строка должна содержать" +
+//                " два операнда и арифметичский знак");
+
+        if (userInput.length <3){
+            throw new Exception("No mathematical operations have been identified");
+        } else if (userInput.length > 3) {
+            throw new Exception("Enter two operands and an arithmetic sign in the format");
+        }
+        String first = userInput[0];
+        String second = userInput[2];
+        String operand = userInput[1];
+
+        boolean firstArabic = Arrays.asList(arabicNumbers).contains(first);
+        boolean secondArabic = Arrays.asList(arabicNumbers).contains(second);
+
+        boolean firstRoman = Arrays.asList(romanNumbers).contains(first);
+        boolean secondRoman = Arrays.asList(romanNumbers).contains(second);
+
+        if ((firstArabic && secondArabic) || (firstRoman && secondRoman)){
+        if (firstArabic && secondArabic) {
+            int firstA = Integer.parseInt(first);
+            int secondA = Integer.parseInt(second);
+            if ((firstA < 1 || firstA > 10) || (secondA < 1 || secondA > 10)) {
+                throw new Exception("Out of range of 1 to 10");
+            } else {
+                Calc arabic = new Calc();
+                int result = arabic.calculateA(firstA, secondA, operand);
+                System.out.println(result);
+            }
+        } else if (firstRoman && secondRoman) {
+            Calc roman = new Calc();
+            int firstR = roman.romanArabic(userInput[0]);
+            int secondR = roman.romanArabic(userInput[2]);
+            if ((firstR < 1 || firstR >10) || (secondR<1 || secondR >10)){
+                throw new Exception("Out of range of 1 to 10");
+            } else {
+                int toRoman = roman.calculateR(firstR,secondR,operand);
+                String result = roman.arabicRoman(toRoman);
+                System.out.println(result);
+            }
+        }
+    } else {
+            throw new Exception("Number raznie");
+        }
+    }
     int romanArabic(String c) {
         HashMap<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
